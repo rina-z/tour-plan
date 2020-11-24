0482b56e7d1ddf2e05a0f74dd1533d4295bb0492
rina-z <rina29zorina@gmail.com>
2020-11-24 14:24:20 +0200

    Created: slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.slider-button--prev',
+    nextEl: '.slider-button--next',
+    
+  },
+
+
+});
